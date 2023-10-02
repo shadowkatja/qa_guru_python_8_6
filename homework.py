f69fcf3bb09a353999ee57f8e451d9899971d632
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-02 19:14:10 +0300

    test_dark_theme_by_time_and_user_choice is done

--- homework.py
+++ homework.py
@@ -32,6 +32,16 @@ def test_dark_theme_by_time_and_user_choice():
     #  но учтите что темная тема может быть включена вручную
 
     is_dark_theme = None
+
+    if not dark_theme_enabled_by_user:
+        if time(hour=22) <= current_time or current_time <= time(hour=6):
+            is_dark_theme = True
+        else:
+            is_dark_theme = False
+    else:
+        is_dark_theme = True
+
+
     assert is_dark_theme is True
 
 
