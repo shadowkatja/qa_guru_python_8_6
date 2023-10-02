f4d8b5be5d7e6f726ec3dc831d70411f49969737
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-02 19:00:50 +0300

    test_dark_theme_by_time is done

--- homework.py
+++ homework.py
@@ -9,6 +9,12 @@ def test_dark_theme_by_time():
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
     is_dark_theme = None
+
+    if (time(hour=22) <= current_time or current_time <= time(hour=6)):
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
+
     assert is_dark_theme is True
 
 
