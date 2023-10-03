68be0f4ace8d5759b630c545bc4b20613ee7b796
Ekaterina Goldinova <shadowkatja@gmail.com>
2023-10-03 11:53:55 +0300

    test_find_suitable_user is done

--- homework.py
+++ homework.py
@@ -54,15 +54,28 @@ def test_find_suitable_user():
         {"name": "Sergey", "age": 24},
         {"name": "Stanislav", "age": 15},
         {"name": "Olga", "age": 45},
-        {"name": "Maria", "age": 18},
+        {"name": "Maria", "age": 18}
     ]
 
     # TODO найдите пользователя с именем "Olga"
     suitable_users = None
+
+    for user in users:
+        if user["name"] == "Olga":
+            suitable_users = user
+            break
+
     assert suitable_users == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
     suitable_users = None
+
+    for user in users:
+        if user["age"] < 20:
+            if suitable_users is None:
+                suitable_users = []
+            suitable_users.append(user)
+
     assert suitable_users == [
         {"name": "Stanislav", "age": 15},
         {"name": "Maria", "age": 18},
